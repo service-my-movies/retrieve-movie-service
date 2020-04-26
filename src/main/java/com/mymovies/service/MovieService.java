88a620b8a7893085f8e2ec0d076f658ad1870656
Ranushan Rachu <ranusha.n@hotmail.fr>
2020-04-26 14:43:38 +0200

    [ORIGIN][MASTER][UPDATE] autowired resttemplate bean

--- src/main/java/com/mymovies/service/MovieService.java
+++ src/main/java/com/mymovies/service/MovieService.java
@@ -3,6 +3,7 @@ package com.mymovies.service;
 import com.mymovies.dto.MovieDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class MovieService implements IMovieService {
 
-	@Value("${resource.api.url}")
+	@Value("${resource.api.url.base}")
 	private String BASE_URL;
 	
 	@Value("${resource.api.url.image}")
@@ -24,7 +25,8 @@ public class MovieService implements IMovieService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MovieService.class);
 	
-	private RestTemplate restTemplate = new RestTemplate();
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public MovieDTO getAPI_Movie(String movie_id) {
 
